27ad9e852a228de9f0b307f4868aadce0cb51d6b
sudo-john-blossom <73914719+sudo-john-blossom@users.noreply.github.com>
2022-11-13 15:47:38 +0000

    asd

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,18 @@ def home(): #name parameter
     #return render_template("index.html") , content = name, r = 2 --> para o caso de ter <name> no route
     #PARA PASSAR UMA LISTA
     return render_template("index.html", content = ["joao", "barradas", "gonzo"])
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"] #name of the input in html
+        return redirect(url_for("user", usr = user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 """
 @app.route('/<name>')
@@ -19,4 +31,4 @@ def admin():
 """
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True) #debug = True --> permite que nao tenhamos que estar sempre a dar re run ao servidor smp que alteramos alguma coisa
